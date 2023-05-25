9c0959a5234ab3d11a06b5c3aec1d8ae2cd884d9
Daniel Guimarães <danguimaraes86@gmail.com>
2023-05-25 19:17:38 -0300

    projeto api-eletrodomesticos

---
+++ api-eletrodomesticos/src/test/java/br/com/fiap/techchallenge/eletrodomesticos/apieletrodomesticos/ApiEletrodomesticosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge.eletrodomesticos.apieletrodomesticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEletrodomesticosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
