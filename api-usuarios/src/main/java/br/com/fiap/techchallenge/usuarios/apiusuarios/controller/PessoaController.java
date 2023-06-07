1c16de8c48e440041e6666998cc81a543987fd40
Daniel Guimarães <danguimaraes86@gmail.com>
2023-06-06 21:14:14 -0300

    adicionar Generics

--- api-usuarios/src/main/java/br/com/fiap/techchallenge/usuarios/apiusuarios/controller/PessoaController.java
+++ api-usuarios/src/main/java/br/com/fiap/techchallenge/usuarios/apiusuarios/controller/PessoaController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -21,33 +20,33 @@ public class PessoaController {
     private PessoaService pessoaService;
 
     @GetMapping
-    public ResponseEntity findAll() {
+    public ResponseEntity<?> findAll() {
         List<Pessoa> listPessoas = pessoaService.findAll();
         return ResponseEntity.ok().body(listPessoas);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity findById(@PathVariable Long id) {
+    public ResponseEntity<?> findById(@PathVariable Long id) {
         Pessoa pessoa = pessoaService.findById(id);
         return pessoa != null ? ResponseEntity.ok().body(pessoa) :
                 ResponseEntity.badRequest().build();
     }
     @PostMapping
-    public ResponseEntity createPessoa(@RequestBody @Valid PessoaDto pessoaDto, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<?> createPessoa(@RequestBody @Valid PessoaDto pessoaDto, UriComponentsBuilder uriBuilder) {
         Pessoa pessoa = pessoaService.create(pessoaDto);
         URI uri = uriBuilder.path("/pessoa/{id}").buildAndExpand(pessoa.getId()).toUri();
         return ResponseEntity.created(uri).body(new PessoaDto(pessoa));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity updatePessoa(@PathVariable Long id, @RequestBody @Valid PessoaDto pessoaDto) {
+    public ResponseEntity<?> updatePessoa(@PathVariable Long id, @RequestBody @Valid PessoaDto pessoaDto) {
         Pessoa pessoa = pessoaService.update(id, pessoaDto);
         return ResponseEntity.ok().body(pessoa);
     }
 
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deletePessoa(@PathVariable Long id) {
+    public ResponseEntity<?> deletePessoa(@PathVariable Long id) {
         return pessoaService.delete(id) ?
                 ResponseEntity.ok().build() :
                 ResponseEntity.badRequest().build();
