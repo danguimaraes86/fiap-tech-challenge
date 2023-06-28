eb880ac08555975bf4137e259f21b633b7ca7e02
Daniel Guimarães <danguimaraes86@gmail.com>
2023-06-28 09:29:57 -0300

    refact: padronização da classe

--- api-eletrodomesticos/src/main/java/br/com/fiap/techchallenge/eletrodomesticos/apieletrodomesticos/controller/EletrodomesticoController.java
+++ api-eletrodomesticos/src/main/java/br/com/fiap/techchallenge/eletrodomesticos/apieletrodomesticos/controller/EletrodomesticoController.java
@@ -1,18 +1,17 @@
 package br.com.fiap.techchallenge.eletrodomesticos.apieletrodomesticos.controller;
 
-import br.com.fiap.techchallenge.eletrodomesticos.apieletrodomesticos.domain.entity.Eletrodomestico;
 import br.com.fiap.techchallenge.eletrodomesticos.apieletrodomesticos.domain.dto.EletrodomesticoDTO;
+import br.com.fiap.techchallenge.eletrodomesticos.apieletrodomesticos.domain.entity.Eletrodomestico;
 import br.com.fiap.techchallenge.eletrodomesticos.apieletrodomesticos.service.EletrodomesticoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/eletrodomestico")
 public class EletrodomesticoController {
 
@@ -20,35 +19,33 @@ public class EletrodomesticoController {
     private EletrodomesticoService service;
 
     @GetMapping
-    public ResponseEntity<?> findAll() {
+    public ResponseEntity<List<Eletrodomestico>> findAll() {
         List<Eletrodomestico> listEletro = service.findAll();
         return ResponseEntity.ok().body(listEletro);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id) {
+    public ResponseEntity<Eletrodomestico> findById(@PathVariable Long id) {
         Eletrodomestico eletro = service.findById(id);
-        return eletro != null ? ResponseEntity.ok().body(eletro) :
-                ResponseEntity.badRequest().build();
+        return ResponseEntity.ok().body(eletro);
     }
+
     @PostMapping
-    public ResponseEntity<?> createEletro(@RequestBody EletrodomesticoDTO eletroDTO, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<EletrodomesticoDTO> createEletro(@RequestBody EletrodomesticoDTO eletroDTO, UriComponentsBuilder uriBuilder) {
         Eletrodomestico eletro = service.create(eletroDTO);
         URI uri = uriBuilder.path("/eletrodomestico/{id}").buildAndExpand(eletro.getId()).toUri();
         return ResponseEntity.created(uri).body(new EletrodomesticoDTO(eletro));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateEletro(@PathVariable Long id, @RequestBody EletrodomesticoDTO eletroDTO) {
+    public ResponseEntity<Eletrodomestico> updateEletro(@PathVariable Long id, @RequestBody EletrodomesticoDTO eletroDTO) {
         Eletrodomestico eletro = service.update(id, eletroDTO);
         return ResponseEntity.ok().body(eletro);
     }
 
-
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteEletro(@PathVariable Long id) {
-        return service.delete(id) ?
-                ResponseEntity.ok().build() :
-                ResponseEntity.badRequest().build();
+    public ResponseEntity<Void> deleteEletro(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
