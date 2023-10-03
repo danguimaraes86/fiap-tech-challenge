d8378773cdcd8ff607263a60128065e3ca079746
Daniel Guimarães <danguimaraes86@gmail.com>
2023-10-03 10:36:43 -0300

    fix: whitespaces e nome do projeto

--- src/main/java/com/example/techChallengeParkimetro/TechChallengeParkimetroApplication.java
+++ src/main/java/com/example/techChallengeParkimetro/TechChallengeParkimetroApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TechChallengeParkimetroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TechChallengeParkimetroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TechChallengeParkimetroApplication.class, args);
+    }
 
 }
