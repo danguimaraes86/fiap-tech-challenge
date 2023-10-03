17486b8f2385bc52058a97543d6cacf6499bc4a6
Daniel Guimarães <danguimaraes86@gmail.com>
2023-10-03 10:37:38 -0300

    add: rotas e services iniciais

--- src/main/java/com/example/techChallengeParkimetro/controllers/CondutorController.java
+++ src/main/java/com/example/techChallengeParkimetro/controllers/CondutorController.java
@@ -1,7 +1,33 @@
 package com.example.techChallengeParkimetro.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.techChallengeParkimetro.entities.Condutor;
+import com.example.techChallengeParkimetro.entities.dtos.CondutorDTO;
+import com.example.techChallengeParkimetro.services.CondutorService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping("/condutor")
 @RestController
 public class CondutorController {
+
+    private final CondutorService condutorService;
+
+    public CondutorController(CondutorService condutorService) {
+        this.condutorService = condutorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CondutorDTO>> findAll() {
+        List<Condutor> condutorList = condutorService.findAll();
+        return ResponseEntity.ok(condutorList.stream().map(Condutor::toDTO).toList());
+    }
+
+    @PostMapping
+    public ResponseEntity<CondutorDTO> createCondutor(@RequestBody @Valid CondutorDTO condutorDTO) {
+        Condutor condutor = condutorService.createCondutor(condutorDTO.toEntity());
+        return ResponseEntity.ok(condutor.toDTO());
+    }
 }
--- src/main/java/com/example/techChallengeParkimetro/services/CondutorService.java
+++ src/main/java/com/example/techChallengeParkimetro/services/CondutorService.java
@@ -1,7 +1,25 @@
 package com.example.techChallengeParkimetro.services;
 
+import com.example.techChallengeParkimetro.entities.Condutor;
+import com.example.techChallengeParkimetro.infra.repositories.CondutorRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CondutorService {
+
+    private final CondutorRepository condutorRepository;
+
+    public CondutorService(CondutorRepository condutorRepository) {
+        this.condutorRepository = condutorRepository;
+    }
+
+    public List<Condutor> findAll() {
+        return condutorRepository.findAll();
+    }
+
+    public Condutor createCondutor(Condutor condutor) {
+        return condutorRepository.save(condutor);
+    }
 }
