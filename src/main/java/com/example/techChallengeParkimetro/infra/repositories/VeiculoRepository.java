fa2642faa5881fbc747c4a478944b2bc31ba4ed4
Daniel Guimarães <danguimaraes86@gmail.com>
2023-10-18 11:57:00 -0300

    add: repository annotation

--- src/main/java/com/example/techChallengeParkimetro/infra/repositories/VeiculoRepository.java
+++ src/main/java/com/example/techChallengeParkimetro/infra/repositories/VeiculoRepository.java
@@ -3,10 +3,12 @@ package com.example.techChallengeParkimetro.infra.repositories;
 import com.example.techChallengeParkimetro.entities.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface VeiculoRepository extends JpaRepository<Veiculo, UUID> {
     Optional<Veiculo> findVeiculoByPlaca(@Param("placa") String placa);
 }
