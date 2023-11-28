cba43545e555878e6b4da9cd5c995fd4f6dc37f3
Daniel Guimarães <danguimaraes86@gmail.com>
2023-11-28 09:46:49 -0300

    add: VideoDTOMock

--- fase-04/src/test/java/br/com/fiap/techchallenge/utils/VideoUtil.java
+++ fase-04/src/test/java/br/com/fiap/techchallenge/utils/VideoUtil.java
@@ -1,8 +1,11 @@
 package br.com.fiap.techchallenge.utils;
 
 import br.com.fiap.techchallenge.domain.Video;
+import br.com.fiap.techchallenge.domain.VideoDTO;
 import com.github.javafaker.Faker;
+import org.bson.types.ObjectId;
 
+import java.time.LocalDateTime;
 import java.util.Locale;
 
 public class VideoUtil {
@@ -11,9 +14,22 @@ public class VideoUtil {
         Faker faker = new Faker(new Locale("pt_BR"));
 
         return new Video(
+                ObjectId.get().toHexString(),
                 faker.witcher().witcher(),
                 faker.witcher().school(),
-                faker.witcher().quote()
+                faker.witcher().quote(),
+                LocalDateTime.now()
+        );
+    }
+
+    public static VideoDTO gerarVideoDTOMock() {
+        Faker faker = new Faker(new Locale("pt_BR"));
+
+        return new VideoDTO(
+                faker.witcher().witcher(),
+                faker.witcher().school(),
+                faker.witcher().quote(),
+                LocalDateTime.now()
         );
     }
 }
