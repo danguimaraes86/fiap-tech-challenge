52bd9dd00885f44fc65e5b2a5c6641048289108d
Daniel Guimarães <danguimaraes86@gmail.com>
2024-01-10 09:56:27 -0300

    add: dependência spring-validation

--- fase-04/src/main/java/br/com/fiap/techchallenge/domain/VideoDTO.java
+++ fase-04/src/main/java/br/com/fiap/techchallenge/domain/VideoDTO.java
@@ -1,10 +1,15 @@
 package br.com.fiap.techchallenge.domain;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public record VideoDTO(
+        @NotBlank
         String titulo,
+        @NotBlank
         String descricao,
+        @NotBlank
         String url,
         LocalDateTime dataPublicacao,
         LocalDateTime ultimaAlteracao
