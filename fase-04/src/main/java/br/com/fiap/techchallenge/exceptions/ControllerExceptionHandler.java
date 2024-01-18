91ab4695c3ec3477df74b137cc33d299fe106601
Daniel Guimarães <danguimaraes86@gmail.com>
2024-01-18 11:21:30 -0300

    add: novos ExceptionHandlers

--- fase-04/src/main/java/br/com/fiap/techchallenge/exceptions/ControllerExceptionHandler.java
+++ fase-04/src/main/java/br/com/fiap/techchallenge/exceptions/ControllerExceptionHandler.java
@@ -1,8 +1,10 @@
 package br.com.fiap.techchallenge.exceptions;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -15,5 +17,15 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(UsuarioNotFoundException.class)
     public ResponseEntity<Void> usuarioNotFoundException() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> methodArgumentTypeMismatchException() {
+        return ResponseEntity.badRequest().body("id com formato incorreto");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> methodArgumentNotValidException() {
+        return ResponseEntity.badRequest().body("dados do vídeo incompleto/incorreto");
     }
 }
