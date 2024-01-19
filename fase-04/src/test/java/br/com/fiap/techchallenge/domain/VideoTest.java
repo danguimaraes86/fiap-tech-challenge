2845c51a09d91bc13b3e384924a88d4aa918374f
Daniel Guimarães <danguimaraes86@gmail.com>
2024-01-19 19:44:13 -0300

    clean: retirar código desnecessário

--- fase-04/src/test/java/br/com/fiap/techchallenge/domain/VideoTest.java
+++ fase-04/src/test/java/br/com/fiap/techchallenge/domain/VideoTest.java
@@ -1,8 +1,5 @@
 package br.com.fiap.techchallenge.domain;
 
-import br.com.fiap.techchallenge.domain.dtos.VideoDTO;
-import br.com.fiap.techchallenge.utils.VideoUtil;
-import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,32 +11,5 @@ class VideoTest {
         Video video = new Video();
         assertThat(video)
                 .isInstanceOf(Video.class);
-    }
-
-    @Test
-    void deveAtualizarDadosVideo() {
-        Video videoFake = VideoUtil.gerarVideoMock();
-        VideoDTO videoDTOFake = VideoUtil.gerarVideoDTOMock();
-
-        Video videoAtualizado = videoFake.update(videoDTOFake);
-
-        assertThat(videoFake).isEqualTo(videoAtualizado);
-        assertThat(videoAtualizado.getId()).isInstanceOf(ObjectId.class);
-        assertThat(videoAtualizado.getTitulo()).isEqualTo(videoDTOFake.titulo());
-        assertThat(videoAtualizado.getUrl()).isEqualTo(videoDTOFake.url());
-        assertThat(videoAtualizado.getDescricao()).isEqualTo(videoDTOFake.descricao());
-        assertThat(videoAtualizado.getDataPublicacao()).isBefore(videoFake.getUltimaAlteracao());
-    }
-
-    @Test
-    void deveRetornarVideoDTO() {
-        Video videoFake = VideoUtil.gerarVideoMock();
-        VideoDTO videoDTO = videoFake.toVideoDTO();
-
-        assertThat(videoDTO.titulo()).isEqualTo(videoFake.getTitulo());
-        assertThat(videoDTO.descricao()).isEqualTo(videoFake.getDescricao());
-        assertThat(videoDTO.url()).isEqualTo(videoFake.getUrl());
-        assertThat(videoDTO.dataPublicacao()).isEqualTo(videoFake.getDataPublicacao());
-        assertThat(videoDTO.ultimaAlteracao()).isEqualTo(videoFake.getUltimaAlteracao());
     }
 }
