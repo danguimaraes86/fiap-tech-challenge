382332b049a20c61664cf9a6b1f0ed62c4e6526c
Daniel Guimarães <danguimaraes86@gmail.com>
2024-02-01 12:44:00 -0300

    add: modulo ms-usuarios

---
+++ fase-05/ms-usuarios/src/test/java/br/com/fiap/techchallenge/usuarios/MsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
