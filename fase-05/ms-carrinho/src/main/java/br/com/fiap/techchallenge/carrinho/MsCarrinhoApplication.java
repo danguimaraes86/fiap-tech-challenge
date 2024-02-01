204231ed909c6612d5e5e5cfad3a0f4f0cb5c9cd
Daniel Guimarães <danguimaraes86@gmail.com>
2024-02-01 12:43:35 -0300

    add: modulo ms-carrinho

---
+++ fase-05/ms-carrinho/src/main/java/br/com/fiap/techchallenge/carrinho/MsCarrinhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge.carrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCarrinhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCarrinhoApplication.class, args);
+    }
+
+}
