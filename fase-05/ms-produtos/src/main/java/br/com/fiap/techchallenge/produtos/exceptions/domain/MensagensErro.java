584f48576fa44b5fcedf7c521662f8e8539c08a6
Daniel Guimarães <danguimaraes86@gmail.com>
2024-02-09 11:36:39 -0300

    add: enum para mensagens de erro

---
+++ fase-05/ms-produtos/src/main/java/br/com/fiap/techchallenge/produtos/exceptions/domain/MensagensErro.java
@@ -0,0 +1,17 @@
+package br.com.fiap.techchallenge.produtos.exceptions.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum MensagensErro {
+    PRODUTO_NAO_ENCONTRADO("produto_id %s não encontrado"),
+    NOME_JA_CADASTRADO("produto_nome %s já cadastrado"),
+    PRECO_ABAIXO_ZERO("produto_preco não pode ser menor que R$ 0,00"),
+    ESTOQUE_INSUFICIENTE("o pedido atual é de %d. estoque atual é %d.");
+
+    private final String mensagem;
+
+    MensagensErro(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
