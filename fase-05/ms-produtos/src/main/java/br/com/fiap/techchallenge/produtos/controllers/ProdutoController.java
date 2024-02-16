83b6f22be3a8d45dffd4fe77a88a6140ca156307
Daniel Guimarães <danguimaraes86@gmail.com>
2024-02-16 16:38:44 -0300

    add: ProdutoController e ExceptionHandler

---
+++ fase-05/ms-produtos/src/main/java/br/com/fiap/techchallenge/produtos/controllers/ProdutoController.java
@@ -0,0 +1,55 @@
+package br.com.fiap.techchallenge.produtos.controllers;
+
+import br.com.fiap.techchallenge.produtos.model.Produto;
+import br.com.fiap.techchallenge.produtos.model.dtos.ProdutoDTO;
+import br.com.fiap.techchallenge.produtos.services.ProdutoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.SortDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<Page<ProdutoDTO>> findAllProdutos(
+            @SortDefault(sort = "nome") Pageable pageable) {
+        Page<Produto> produtos = produtoService.findAllProdutos(pageable);
+        return ResponseEntity.ok(produtos.map(Produto::toProdutoDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> findProdutoById(@PathVariable String id) {
+        Produto produto = produtoService.findProdutoById(id);
+        return ResponseEntity.ok(produto.toProdutoDTO());
+    }
+
+    @GetMapping("/busca")
+    public ResponseEntity<Page<ProdutoDTO>> findProdutoByNomeByDescricao(
+            @SortDefault(sort = "nome") Pageable pageable,
+            @RequestParam(required = false, defaultValue = "") String nome,
+            @RequestParam(required = false, defaultValue = "") String descricao) {
+        Page<Produto> produtos = produtoService.findProdutosNomeDescricao(pageable, nome, descricao);
+        return ResponseEntity.ok(produtos.map(Produto::toProdutoDTO));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> insertProduto(@RequestBody @Valid ProdutoDTO produtoDTO) {
+        Produto produto = produtoService.insertProduto(produtoDTO);
+        return ResponseEntity.ok(produto.toProdutoDTO());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> updateEstoqueProduto(
+            @PathVariable String id, @RequestParam Long alteracaoEstoque) {
+        Produto produto = produtoService.updateProdutoEstoque(id, alteracaoEstoque);
+        return ResponseEntity.ok(produto.toProdutoDTO());
+    }
+}
---
+++ fase-05/ms-produtos/src/main/java/br/com/fiap/techchallenge/produtos/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,87 @@
+package br.com.fiap.techchallenge.produtos.exceptions;
+
+import br.com.fiap.techchallenge.produtos.exceptions.domain.EstoqueInsuficienteException;
+import br.com.fiap.techchallenge.produtos.exceptions.domain.ProdutoJaCadastradoException;
+import br.com.fiap.techchallenge.produtos.exceptions.domain.ProdutoNaoEncontradoException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    private static Map<String, String> getFieldErrors(MethodArgumentNotValidException exception) {
+        return exception.getFieldErrors()
+                .stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DefaultError> methodArgumentNotValidException(
+            MethodArgumentNotValidException exception, HttpServletRequest request
+    ) {
+        return ResponseEntity.unprocessableEntity().body(
+                new DefaultError(
+                        LocalDateTime.now(),
+                        HttpStatus.UNPROCESSABLE_ENTITY,
+                        request.getContextPath(),
+                        getFieldErrors(exception)
+                )
+        );
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<DefaultError> missingServletRequestParameterException(
+            MissingServletRequestParameterException exception, HttpServletRequest request
+    ) {
+        return ResponseEntity.badRequest().body(
+                new DefaultError(
+                        LocalDateTime.now(),
+                        HttpStatus.BAD_REQUEST,
+                        request.getContextPath(),
+                        Collections.singletonMap(
+                                exception.getParameterName(), exception.getBody().getDetail())
+                )
+        );
+    }
+
+    @ExceptionHandler(ProdutoNaoEncontradoException.class)
+    public ResponseEntity<DefaultError> produtoNaoEncontradoException() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(ProdutoJaCadastradoException.class)
+    public ResponseEntity<DefaultError> produtoJaCadastradoException(
+            ProdutoJaCadastradoException exception, HttpServletRequest request
+    ) {
+        return ResponseEntity.unprocessableEntity().body(
+                new DefaultError(
+                        LocalDateTime.now(),
+                        HttpStatus.UNPROCESSABLE_ENTITY,
+                        request.getContextPath(),
+                        Collections.singletonMap("nome", exception.getMessage())
+                ));
+    }
+
+    @ExceptionHandler(EstoqueInsuficienteException.class)
+    public ResponseEntity<DefaultError> estoqueInsuficienteException(
+            EstoqueInsuficienteException exception, HttpServletRequest request
+    ) {
+        return ResponseEntity.unprocessableEntity().body(
+                new DefaultError(
+                        LocalDateTime.now(),
+                        HttpStatus.UNPROCESSABLE_ENTITY,
+                        request.getContextPath(),
+                        Collections.singletonMap("estoque", exception.getMessage())
+                ));
+    }
+}
