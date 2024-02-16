783001e31a6fa13bb138dc0ddb5a1f38052b58d8
Daniel Guimarães <danguimaraes86@gmail.com>
2024-02-16 16:42:07 -0300

    refact: melhorias nos testes

--- fase-05/ms-produtos/src/test/java/br/com/fiap/techchallenge/produtos/services/ProdutoServiceTest.java
+++ fase-05/ms-produtos/src/test/java/br/com/fiap/techchallenge/produtos/services/ProdutoServiceTest.java
@@ -1,7 +1,6 @@
 package br.com.fiap.techchallenge.produtos.services;
 
 import br.com.fiap.techchallenge.produtos.exceptions.domain.EstoqueInsuficienteException;
-import br.com.fiap.techchallenge.produtos.exceptions.domain.PrecoAbaixoZeroException;
 import br.com.fiap.techchallenge.produtos.exceptions.domain.ProdutoJaCadastradoException;
 import br.com.fiap.techchallenge.produtos.exceptions.domain.ProdutoNaoEncontradoException;
 import br.com.fiap.techchallenge.produtos.model.Produto;
@@ -36,8 +35,8 @@ class ProdutoServiceTest {
     @Mock
     private ProdutoRepository produtoRepository;
 
-    private static Produto getProdutoMock(String id, Double preco, Long estoque) {
-        return new Produto(id, RandomString.make(), RandomString.make(), preco, estoque);
+    private static Produto getProdutoMock(Double preco, Long estoque) {
+        return new Produto(RandomString.make(), RandomString.make(), preco, estoque);
     }
 
     @BeforeEach
@@ -59,11 +58,11 @@ class ProdutoServiceTest {
             Page<Produto> produtoPageFake = new PageImpl<>(Collections.emptyList());
             when(produtoRepository.findAll(any(Pageable.class))).thenReturn(produtoPageFake);
 
-            Page<Produto> videoPage = produtoService.findAllProdutos(Pageable.unpaged());
+            Page<Produto> produtoPage = produtoService.findAllProdutos(Pageable.unpaged());
             verify(produtoRepository, times(1)).findAll(Pageable.unpaged());
 
-            assertThat(videoPage).isInstanceOf(Page.class);
-            assertThat(videoPage.getContent()).isEmpty();
+            assertThat(produtoPage).isInstanceOf(Page.class);
+            assertThat(produtoPage.getContent()).isEmpty();
         }
 
         @Test
@@ -74,28 +73,62 @@ class ProdutoServiceTest {
                     mock(Produto.class)));
             when(produtoRepository.findAll(any(Pageable.class))).thenReturn(produtoPageFake);
 
-            Page<Produto> videoPage = produtoService.findAllProdutos(Pageable.unpaged());
+            Page<Produto> produtoPage = produtoService.findAllProdutos(Pageable.unpaged());
             verify(produtoRepository, times(1)).findAll(Pageable.unpaged());
 
-            assertThat(videoPage).isInstanceOf(Page.class);
-            assertThat(videoPage.getTotalElements()).isEqualTo(produtoPageFake.getTotalElements());
+            assertThat(produtoPage).isInstanceOf(Page.class);
+            assertThat(produtoPage.getTotalElements()).isEqualTo(produtoPageFake.getTotalElements());
         }
 
         @Test
         void deveBuscarProdutoPorId() {
             String id = UUID.randomUUID().toString();
-            Produto produtoMock = getProdutoMock(id, 7.0, 7L);
+            Produto produtoMock = getProdutoMock(7.0, 7L);
             when(produtoRepository.findById(anyString())).thenReturn(Optional.of(produtoMock));
 
             Produto produto = produtoService.findProdutoById(id);
             verify(produtoRepository, times(1)).findById(id);
 
             assertThat(produto).isEqualTo(produtoMock);
-            assertThat(produto.getId()).isEqualTo(produtoMock.getId());
             assertThat(produto.getNome()).isEqualTo(produtoMock.getNome());
             assertThat(produto.getDescricao()).isEqualTo(produtoMock.getDescricao());
             assertThat(produto.getPreco()).isEqualTo(produtoMock.getPreco());
             assertThat(produto.getEstoque()).isEqualTo(produtoMock.getEstoque());
+        }
+
+        @Test
+        void deveBuscarProdutoPor_NomeOuDescricao_RetornaPageableVazio() {
+            Page<Produto> produtoPageFake = new PageImpl<>(Collections.emptyList());
+            when(produtoRepository.findByNomeContainsIgnoreCaseAndDescricaoContainsIgnoreCase(
+                    any(Pageable.class), anyString(), anyString())).thenReturn(produtoPageFake);
+
+            Page<Produto> produtoPage = produtoService.findProdutosNomeDescricao(
+                    Pageable.unpaged(), "", "");
+            verify(produtoRepository, times(1))
+                    .findByNomeContainsIgnoreCaseAndDescricaoContainsIgnoreCase(
+                            Pageable.unpaged(), "", "");
+
+            assertThat(produtoPage).isInstanceOf(Page.class);
+            assertThat(produtoPage.getContent()).isEmpty();
+        }
+
+        @Test
+        void deveBuscarProdutosPor_NomeOuDescricao_RetornaPageable() {
+            Page<Produto> produtoPageFake = new PageImpl<>(List.of(
+                    mock(Produto.class),
+                    mock(Produto.class),
+                    mock(Produto.class)));
+            when(produtoRepository.findByNomeContainsIgnoreCaseAndDescricaoContainsIgnoreCase(
+                    any(Pageable.class), anyString(), anyString())).thenReturn(produtoPageFake);
+
+            Page<Produto> produtoPage = produtoService.findProdutosNomeDescricao(
+                    Pageable.unpaged(), "", "");
+            verify(produtoRepository, times(1))
+                    .findByNomeContainsIgnoreCaseAndDescricaoContainsIgnoreCase(
+                            Pageable.unpaged(), "", "");
+
+            assertThat(produtoPage).isInstanceOf(Page.class);
+            assertThat(produtoPage.getTotalElements()).isEqualTo(produtoPageFake.getTotalElements());
         }
     }
 
@@ -104,7 +137,7 @@ class ProdutoServiceTest {
 
         @Test
         void deveInserirProduto_comSucesso() {
-            Produto produtoMock = new Produto(RandomString.make(), RandomString.make(), 7.0);
+            Produto produtoMock = getProdutoMock(10.0, 0L);
             ProdutoDTO produtoDTO = produtoMock.toProdutoDTO();
             when(produtoRepository.save(any(Produto.class))).thenReturn(produtoMock);
 
@@ -112,10 +145,11 @@ class ProdutoServiceTest {
             verify(produtoRepository, times(1)).save(any(Produto.class));
 
             assertThat(produto).isInstanceOf(Produto.class);
+            assertThat(produto.getId()).isEqualTo(produtoMock.getId());
             assertThat(produto.getNome()).isEqualTo(produtoDTO.nome());
             assertThat(produto.getDescricao()).isEqualTo(produtoDTO.descricao());
-            assertThat(produto.getPreco()).isEqualTo(produtoDTO.preco());
-            assertThat(produto.getEstoque()).isZero();
+            assertThat(produto.getPreco()).isEqualTo(produtoDTO.preco().orElseThrow());
+            assertThat(produto.getEstoque()).isEqualTo(produtoDTO.estoque().orElseThrow());
         }
     }
 
@@ -125,7 +159,7 @@ class ProdutoServiceTest {
         @Test
         void deveAlterarEstoque_comSucesso() {
             String id = UUID.randomUUID().toString();
-            Produto produtoMock = getProdutoMock(id, 10.0, 10L);
+            Produto produtoMock = getProdutoMock(10.0, 10L);
             Long alteracaoEstoque = (-7L);
             when(produtoRepository.findById(anyString())).thenReturn(Optional.of(produtoMock));
             when(produtoRepository.save(any(Produto.class))).thenReturn(produtoMock);
@@ -135,22 +169,6 @@ class ProdutoServiceTest {
             verify(produtoRepository, times(1)).save(any(Produto.class));
 
             assertThat(produto.getEstoque()).isEqualTo(produtoMock.getEstoque());
-        }
-    }
-
-    @Nested
-    class RemoverProduto {
-
-        @Test
-        void deveRemoverProduto_comSucesso() {
-            String id = UUID.randomUUID().toString();
-            Produto produtoMock = getProdutoMock(id, 10.0, 10L);
-            when(produtoRepository.findById(anyString())).thenReturn(Optional.of(produtoMock));
-            doNothing().when(produtoRepository).delete(any(Produto.class));
-
-            produtoService.deleteProduto(id);
-            verify(produtoRepository, times(1)).findById(anyString());
-            verify(produtoRepository, times(1)).delete(any(Produto.class));
         }
     }
 
@@ -170,8 +188,7 @@ class ProdutoServiceTest {
 
         @Test
         void deveLancarExcecao_inserirProduto_nomeJaCadastrado() {
-            String id = UUID.randomUUID().toString();
-            Produto produtoMock = getProdutoMock(id, 10.0, 10L);
+            Produto produtoMock = getProdutoMock(10.0, 10L);
             ProdutoDTO produtoDTOMock = produtoMock.toProdutoDTO();
             when(produtoRepository.findByNomeIgnoreCase(anyString())).thenReturn(Optional.of(produtoMock));
 
@@ -183,23 +200,9 @@ class ProdutoServiceTest {
         }
 
         @Test
-        void deveLancarExcecao_inserirProduto_precoMenorZero() {
-            String id = UUID.randomUUID().toString();
-            Produto produtoMock = getProdutoMock(id, -2.0, 10L);
-            ProdutoDTO produtoDTOMock = produtoMock.toProdutoDTO();
-            when(produtoRepository.findByNomeIgnoreCase(anyString())).thenReturn(Optional.empty());
-
-            assertThatThrownBy(() -> produtoService.insertProduto(produtoDTOMock))
-                    .isInstanceOf(PrecoAbaixoZeroException.class)
-                    .hasMessage(PRECO_ABAIXO_ZERO.getMensagem());
-            verify(produtoRepository, times(1)).findByNomeIgnoreCase(anyString());
-            verify(produtoRepository, never()).save(produtoMock);
-        }
-
-        @Test
         void deveLancarExcecao_alterarEstoque_produtoSemEstoque() {
             String id = UUID.randomUUID().toString();
-            Produto produtoMock = getProdutoMock(id, 10.0, 10L);
+            Produto produtoMock = getProdutoMock(10.0, 10L);
             Long alterarEstoque = (-13L);
             when(produtoRepository.findById(anyString())).thenReturn(Optional.of(produtoMock));
 
@@ -209,18 +212,6 @@ class ProdutoServiceTest {
                             alterarEstoque, produtoMock.getEstoque()));
             verify(produtoRepository, times(1)).findById(anyString());
             verify(produtoRepository, never()).save(produtoMock);
-        }
-
-        @Test
-        void deveLancarExcecao_removerProduto_produtoNaoEncontrado() {
-            String id = UUID.randomUUID().toString();
-            when(produtoRepository.findById(anyString())).thenReturn(Optional.empty());
-
-            assertThatThrownBy(() -> produtoService.deleteProduto(id))
-                    .isInstanceOf(ProdutoNaoEncontradoException.class)
-                    .hasMessage(String.format("produto_id %s não encontrado", id));
-            verify(produtoRepository, times(1)).findById(anyString());
-            verify(produtoRepository, never()).delete(any(Produto.class));
         }
     }
 }
