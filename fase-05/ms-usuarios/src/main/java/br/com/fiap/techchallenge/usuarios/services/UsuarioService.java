fef73d306a96cf162672acd72c5568395d55491a
Daniel Guimarães <danguimaraes86@gmail.com>
2024-02-21 18:34:39 -0300

    add: validação de createUsuario e encodedPassword

--- fase-05/ms-usuarios/src/main/java/br/com/fiap/techchallenge/usuarios/services/UsuarioService.java
+++ fase-05/ms-usuarios/src/main/java/br/com/fiap/techchallenge/usuarios/services/UsuarioService.java
@@ -1,12 +1,16 @@
 package br.com.fiap.techchallenge.usuarios.services;
 
+import br.com.fiap.techchallenge.usuarios.exceptions.UsuarioJaCadastradoException;
 import br.com.fiap.techchallenge.usuarios.exceptions.UsuarioNaoEncontradoException;
+import br.com.fiap.techchallenge.usuarios.models.Role;
 import br.com.fiap.techchallenge.usuarios.models.Usuario;
 import br.com.fiap.techchallenge.usuarios.models.dtos.UsuarioDTO;
 import br.com.fiap.techchallenge.usuarios.repositories.UsuarioRespository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -14,14 +18,15 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRespository usuarioRespository;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public Page<Usuario> findAllUsuarios(Pageable pageable) {
         return usuarioRespository.findAll(pageable);
     }
 
-    public Usuario findUsuarioById(String id) {
-        return usuarioRespository.findById(id).orElseThrow(
-                () -> new UsuarioNaoEncontradoException("usuario_id não encontrado")
+    public Usuario findUsuarioByEmail(String email) {
+        return usuarioRespository.findById(email).orElseThrow(() ->
+                new UsuarioNaoEncontradoException("usuario_email não encontrado")
         );
     }
 
@@ -29,8 +34,14 @@ public class UsuarioService {
         return usuarioRespository.findByEmailIgnoreCaseOrNomeLikeIgnoreCase(pageable, email, nome);
     }
 
-    public Usuario insertUsuario(UsuarioDTO usuarioDTO) {
-        Usuario usuario = new Usuario(usuarioDTO.email(), usuarioDTO.nome(), usuarioDTO.password());
+    public Usuario createUsuario(UsuarioDTO usuarioDTO) {
+        usuarioRespository.findById(usuarioDTO.email()).ifPresent(usuario -> {
+            throw new UsuarioJaCadastradoException(
+                    String.format("usario_email %s já existe", usuario.getEmail()));
+        });
+        String encodedPassword = passwordEncoder.encode(usuarioDTO.password());
+        Usuario usuario = new Usuario(
+                usuarioDTO.email(), usuarioDTO.nome(), encodedPassword, Role.USER);
         return usuarioRespository.save(usuario);
     }
 }
