55982e1b2fb41a7e8e98d9815c2cf85a54f936d3
Hugo Souza Araujo <hsaraujo98@outlook.com>
2024-02-28 22:10:39 -0300

    Escrevendo testes unitarios

--- fase-05/ms-carrinho/src/test/java/br/com/fiap/techchallenge/carrinho/services/CarrinhoServiceTest.java
+++ fase-05/ms-carrinho/src/test/java/br/com/fiap/techchallenge/carrinho/services/CarrinhoServiceTest.java
@@ -5,6 +5,7 @@ import br.com.fiap.techchallenge.carrinho.entities.Produtos;
 import br.com.fiap.techchallenge.carrinho.functions.EstoquePedidoProducer;
 import br.com.fiap.techchallenge.carrinho.repository.CarrinhoFinalizadoRepository;
 import br.com.fiap.techchallenge.carrinho.repository.CarrinhoRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
@@ -51,13 +53,44 @@ public class CarrinhoServiceTest {
     }
 
     @Test
-    void deveAdicionarItemOuCriarCarrinho(){
+    void deveCriarCarrinhoAoNaoEncontrarUmAberto(){
         Produtos p = geraProduto();
         Mockito.when(carrinhoService.addItemsOuCriarCarrinho("1", p)).thenReturn(new CarrinhoAberto());
     }
 
+    @Test
+    void deveAdicionarItemAoCarrinhoAberto(){
+        Produtos p = geraProduto();
+        CarrinhoAberto carrinhoAberto = new CarrinhoAberto("1", p);
+        Mockito.when(carrinhoRepository.save(carrinhoAberto)).thenReturn(carrinhoAberto);
+        Mockito.when(carrinhoRepository.findById("1")).thenReturn(Optional.of(carrinhoAberto));
+        var car = carrinhoService.addItemsOuCriarCarrinho(carrinhoAberto.getUsuarioId(), p);
+        assertThat(car).isNotNull();
+        Assertions.assertEquals( "1", car.getUsuarioId());
+    }
+
+    @Test
+    void deveLançarExceçãoAoNãoAcharEstoque(){
+        CarrinhoAberto car = geraCarrinhoAberto();
+        Mockito.when(carrinhoRepository.save(car)).thenReturn(car);
+        Mockito.when(carrinhoRepository.findById("1")).thenReturn(Optional.of(car));
+        assertThatThrownBy(() -> carrinhoService.efetuandoCompraDoCarrrinho(car.getUsuarioId()))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void deveDeletarCarrinho(){
+        var car = geraCarrinhoAberto();
+        carrinhoService.deletarCarrinho(car.getUsuarioId());
+    }
+
+    CarrinhoAberto geraCarrinhoAberto(){
+        Produtos p = geraProduto();
+        CarrinhoAberto car = new CarrinhoAberto("1", p);
+        return car;
+    }
     Produtos geraProduto(){
-        Produtos produtos = new Produtos("1", 10l);
+        Produtos produtos = new Produtos("1", 2l);
         return produtos;
     }
 }
