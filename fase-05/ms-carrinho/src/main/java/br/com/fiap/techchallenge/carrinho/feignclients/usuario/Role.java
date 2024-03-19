f6899df93405a39f8950370a682519c858caf46c
Daniel Guimarães <danguimaraes86@gmail.com>
2024-03-19 17:29:58 -0300

    [Carrinho] refact: remover Usuario do serviço

--- fase-05/ms-carrinho/src/main/java/br/com/fiap/techchallenge/carrinho/feignclients/usuario/Role.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.techchallenge.carrinho.feignclients.usuario;
-
-public enum Role {
-    ADMIN, USER
-}
--- fase-05/ms-carrinho/src/main/java/br/com/fiap/techchallenge/carrinho/feignclients/usuario/Usuario.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.techchallenge.carrinho.feignclients.usuario;
-
-public record Usuario(
-        String email,
-        String nome,
-        String password,
-        Role role
-) {
-}
